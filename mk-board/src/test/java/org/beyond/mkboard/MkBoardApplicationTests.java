b2e9d585142b718230477581948c6d01ebfff2e8
zkvn1103@naver.com <zkvn1103@naver.com>
2025-11-27 14:48:32 +0900

    init

---
+++ mk-board/src/test/java/org/beyond/mkboard/MkBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.beyond.mkboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
