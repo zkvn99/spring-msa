4cf1cffabc8491d6f2483c536a0d62007807a6b3
zkvn1103@naver.com <zkvn1103@naver.com>
2025-12-09 09:21:11 +0900

    edit

---
+++ mk-board/service/article/src/main/java/minuk/board/article/controller/HealthController.java
@@ -0,0 +1,15 @@
+package minuk.board.article.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+
+  @GetMapping("/health")
+  public String health() { return "Jenkins pipieline Day2 Test"; }
+}
